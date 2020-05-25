e7160160f32af544a61b2230ca96f75c8e82e80f
Bhagyalakshmi <bhagyan140568iiitn@gmail.com>
2020-05-25 12:05:20 +0530

    FizzBuzz

---
+++ TDD/src/fizzbuzz.js
@@ -0,0 +1,15 @@
+function fizzbuzz(num) {
+  if (num % 3 == 0 && num % 5 == 0) {
+    return "FizzBuzz";
+  }
+  if (num % 3 == 0) {
+    return "Fizz";
+  }
+  if (num % 5 == 0) {
+    return "Buzz";
+  } else {
+    return num;
+  }
+}
+
+module.exports = { fizzbuzz };
