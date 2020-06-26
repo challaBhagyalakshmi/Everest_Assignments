4c2602fb2cebc0704051ddde265061efc8194fba
Bhagyalakshmi <bhagyan140568iiitn@gmail.com>
2020-06-26 12:56:30 +0530

    testcases of api for top10 countries who is having more covid recovered_cases in the world

---
+++ covid19/test/apis/recoveredtop10.test.js
@@ -0,0 +1,15 @@
+const request = require("supertest");
+const App = require("../../src/server");
+
+const app = App.app;
+
+describe("Top 10 countries who is having highest recovered cases in world", () => {
+  test("it should return top 10 countries ", () => {
+    const response = request(app)
+      .get("/recovered/top10")
+      .expect((res) => {
+        expect(res.status).toBe(200);
+        expect(res.no_of_records).toBe(10);
+      });
+  });
+});
